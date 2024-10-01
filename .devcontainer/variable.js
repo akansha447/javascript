d37d20970db0216048dee07f3b84c10a245ebfaf
akansha447 <akanshagaur777@gmail.com>
2024-10-01 05:10:57 +0000

    container config done

--- .devcontainer/variable.js
+++ .devcontainer/variable.js
@@ -6,6 +6,9 @@ var employeePassword = "8218"
 employeeEmail = "r@gmail.com"
 var employeePassword = "333"
 
-console.log([accountId,employeeEmail,employeeEmail]);
+console.log([
+    accountId,
+    employeeEmail,
+    employeeEmail]);
 //console.log(employeeEmail);
 //console.log(employeeEmail);
