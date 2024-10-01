82fc34aed1f098e6de0870ec52cfffd2efc7974d
akansha447 <akanshagaur777@gmail.com>
2024-10-01 05:09:46 +0000

    container config done

--- .devcontainer/variable.js
+++ .devcontainer/variable.js
@@ -2,9 +2,10 @@ const accountId = 12345
 let employeeEmail = "ak@gmail.com"
 var employeePassword = "8218"
 
- employeeEmail = "r@gmail.com"
- var employeePassword = "333"
+//const accountId = 99999
+employeeEmail = "r@gmail.com"
+var employeePassword = "333"
 
-console.log(accountId);
-console.log(employeeEmail);
-console.log(employeePassword);
+console.log([accountId,employeeEmail,employeeEmail]);
+//console.log(employeeEmail);
+//console.log(employeeEmail);
