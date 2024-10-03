8ca551dac0f15a19980bbd713c65dcd6ca654169
akansha447 <akanshagaur777@gmail.com>
2024-10-03 10:57:57 +0000

    container config data

---
+++ .devcontainer/Datatypes.js
@@ -0,0 +1,34 @@
+// DATA TYPE Convertion
+
+/* let newNumber = 258
+console.log(newNumber);
+console.log(typeof newNumber); */
+
+/*let newNumber = "888abc"
+console.log(newNumber);
+console.log(typeof newNumber); */
+
+/*let newNumber = "888abc"
+let valueInNumber = Number(newNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// boolean data type
+
+/*let score = true
+console.log(score);
+
+let name = "akansha"
+console.log(name); */
+
+// conversation 
+
+/*let data = 99
+console.log(data);
+let stringNumber = String(data)
+console.log(stringNumber);
+console.log(typeof stringNumber); */
+
+// Comparison
+
+console.log(2===2);
