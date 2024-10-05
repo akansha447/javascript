efac743a6a0d3d34151cacca96694bd92d4c0c92
akansha447 <akanshagaur777@gmail.com>
2024-10-05 08:43:10 +0000

    container config data

--- .devcontainer/number.js
+++ .devcontainer/number.js
@@ -5,7 +5,7 @@ const xscore = new Number(500)
 console.log(xscore);
 
 console.log(xscore.toString().length);
-console.log(typeof xscore);
+//console.log(typeof xscore);
 
 //console.log(xscore.toString().charAt);
 
