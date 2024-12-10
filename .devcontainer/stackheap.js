c75b054717ec6a7cc495b11939b28565c5264787
akansha447 <akanshagaur777@gmail.com>
2024-12-10 10:17:48 +0000

    container config done

---
+++ .devcontainer/stackheap.js
@@ -0,0 +1,20 @@
+let myinstaaccount = "iamakansha"
+
+let anotheraccount = myinstaaccount
+anotheraccount = "akanshagaur"
+console.log(myinstaaccount); 
+console.log(anotheraccount);    
+
+let userOne = {
+    userName: "akanshagaur",
+    email:"aka@gmail.com"
+}
+let usertwo = {
+    userName:"utkarsh",
+    email:"ut@gmail.com"
+}
+let userthree = usertwo
+usertwo.email ="utkarsh@gmail.com"
+console.log(userOne);
+console.log(usertwo.email);
+console.log(userthree.email);
