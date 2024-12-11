2526f50b05ecd840810dde14812e65c80cdb4b7e
akansha447 <akanshagaur777@gmail.com>
2024-12-11 10:53:52 +0000

    container config done

--- .devcontainer/array.js
+++ .devcontainer/array.js
@@ -5,5 +5,5 @@ console.log(myArray);
 const myGod =["radhe","ram"]
 myGod.push("Ravan")
 console.log(myGod);
-//myHero.concat(myGod)
-//console.log(myHero);
+myArray.concat(myGod)
+console.log(myHero);
