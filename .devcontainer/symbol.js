6d7fa3ca22a7fd110e9c914449c0e6df44bf6051
akansha447 <akanshagaur777@gmail.com>
2024-12-17 05:26:22 +0000

    container config done

---
+++ .devcontainer/symbol.js
@@ -0,0 +1,9 @@
+const JsUser= {
+    name : "akansha gaur",
+    age : 24,
+    location: "Delhi",
+    isloggedin : false,
+    LastLoginDays : ["Mon","Sat"]
+}
+console.log(JsUser.email);
+console.log(JsUser["eamil"]);
