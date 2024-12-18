634e885aef64c119ffaf975e3d878882a411afc9
akansha447 <akanshagaur777@gmail.com>
2024-12-18 06:55:52 +0000

    container config done

---
+++ .devcontainer/object.js
@@ -0,0 +1,10 @@
+// Objects
+
+const JsUser = {
+    name : "akansha",
+    project : "pms",
+    duration : "sixMonths",
+    isLoggedIn : false,
+    LastLoginDays : ["Monday","Saturday"]
+}
+console.log(JsUser.name)
